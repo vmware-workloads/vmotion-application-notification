2624945b80d47d3a79d4ae7cbe924d966e618b2c
Dharmesh Bhatt <48768405+darkmesh-b@users.noreply.github.com>
2024-09-18 12:05:02 +0100

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -5,13 +5,13 @@ echo "# VMNOTIFICATION INSTALL                 #"
 echo "##########################################"
 echo
 
-FILENAME="vmnotification-v*"
+FILENAME='vmnotification-v'
 URL="https://api.github.com/repos/vmware-workloads/vmotion-application-notification/releases/latest"
 
 browser_url=$(curl -s "$URL" | grep browser_download_url)
 [[ $(echo $?) -eq 0 ]] && echo \>\>Obtained URL || echo "error obtaining URL"
 
-directory=$(echo $browser_url | awk '{print $NF}' | xargs curl -sL | tar zxv 2> >(grep -o $FILENAME) | sort -u)
+directory=$(echo $browser_url | awk '{print $NF}' | xargs curl -sL | tar zxv 2> >(grep -o $FILENAME*) | sort -u)
 
 pushd "$directory"
 echo "Calling install.sh"
