8905e776393f56b2ce236fd427f0e3fa40a4c400
Dharmesh Bhatt <48768405+darkmesh-b@users.noreply.github.com>
2024-10-14 11:28:58 +0100

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -26,7 +26,6 @@ fi
 # Download and extract
 printf "Download & extract $FILENAME..."
 directory=$(curl -sL $browser_url | tar zxv |grep -o $FILENAME*| sort -u)
-echo $directory
 echo "OK"
 
 # Run the installer
